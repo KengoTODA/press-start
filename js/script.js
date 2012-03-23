ad8931d7b66c013d3f0c40d49767036df292be81
eller86 <skypencil@gmail.com>
2012-03-23 22:48:43 +0800

    changing index page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-/* Author:
+var form = $('form#download');
+form.submit(function(e) {
+  var version = $('input:radio[name=version]:checked').val(),
+      plugin = $('#plugin').val() || [],
+      file_path = 'download/' + version;
+  e.preventDefault();
 
-*/
-
-
-
-
-
-
-
+  if (plugin.length > 0) { file_path += '_' + plugin.join('-'); }
+  file_path += '.zip';
+  document.location = file_path;
+  return false;
+});
